f983794d52b1439d7ae165f06117dd7f2cf894f9
Jacopo Fiorenza <jacopo.f91@gmail.com>
2014-11-11 17:39:57 +0100

    Added basic animation

---
+++ Assets/Gameplay/scripts/jellyfish/movement.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+	Animator an = null;
+	float speed = 2f;
+	float rotationSpeed = 120f;
+	bool i_am_moving = false;
+	Rigidbody2D rigidBody;
+
+	#region READ_KEYS
+	bool getLeft(){
+		return Input.GetKey (KeyCode.LeftArrow);
+	}
+	
+	bool getRight(){
+		return Input.GetKey (KeyCode.RightArrow);
+	}
+	
+	bool getThrust(){
+		return Input.GetKey (KeyCode.UpArrow);
+	}
+	
+	bool getFire(){
+		return Input.GetKeyDown (KeyCode.LeftControl);
+	}
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> () as Rigidbody2D;
+		an = GetComponent<Animator> () as Animator;
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (getLeft ()) {
+			transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+		}
+
+		if (getRight ()) {
+			transform.Rotate(Vector3.forward * (-rotationSpeed) * Time.deltaTime);
+		}
+
+		if (getThrust ()) {
+			//			transform.position += transform.up * speed * Time.deltaTime;
+			rigidBody.AddForce(transform.up*speed);
+			if (!i_am_moving){
+				i_am_moving = true;
+				an.SetBool("is_moving",true);
+			}
+		}
+		else {
+			if (i_am_moving){
+				i_am_moving = false;
+				an.SetBool("is_moving",false);
+			}
+		}
+
+		if (/*canShoot &&*/ getFire ()) {
+			//GameObject missile = Instantiate(prefabMissile, transform.position + transform.up * 0.45f, transform.rotation) as GameObject;
+			
+			//canShoot = false;
+			//StartCoroutine(WaitToShoot());
+		}
+	
+	}
+}
---
+++ Assets/Gameplay/scripts/light/lightPulse.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightPulse : MonoBehaviour {
+	public float duration = 2.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		light.range = 15.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var phi = Time.time / duration * 2 * Mathf.PI;
+		// get cosine and transform from -1..1 to 0..1 range
+		var amplitude = Mathf.Cos( phi ) * 0.5 + 0.5;
+		light.range = (float) amplitude;
+	}
+}
