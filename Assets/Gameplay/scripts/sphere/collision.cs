1870597b19f6be5342bd4a4885b8fa05a37e5190
AmedeoAsnaghi <amedeo.asnaghi@gmail.com>
2014-12-09 16:29:32 +0100

    Added Spheres with explosion effect

---
+++ Assets/Gameplay/scripts/sphere/collision.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collision : MonoBehaviour {
+	public float stepExplosion;
+	public float secondToExplode;
+	public int maxParticles;
+
+	ParticleSystem[] ps;
+	bool explode;
+
+	// Use this for initialization
+	void Start () {
+		ps = GetComponentsInChildren<ParticleSystem> (false) as ParticleSystem[];
+		explode = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (explode) {
+			ps[0].startLifetime-=stepExplosion;
+			ps[1].startLifetime-=stepExplosion;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		if (other.gameObject.tag == "Player") {
+			explode = true;
+			ps[2].maxParticles = maxParticles;
+			StartCoroutine(WaitToExplode());
+		} 
+	}
+
+	IEnumerator WaitToExplode()
+	{
+		yield return new WaitForSeconds(secondToExplode);
+		Destroy (gameObject);
+	}
+}
