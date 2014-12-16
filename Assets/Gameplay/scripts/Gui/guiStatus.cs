8c6a83965d7dd958d6edf03a59dcf80eab5dad4f
Jacopo Fiorenza <jacopo.f91@gmail.com>
2014-12-16 17:39:29 +0100

    Modified lifebar / animation

--- Assets/Gameplay/scripts/Gui/guiStatus.cs
+++ Assets/Gameplay/scripts/Gui/guiStatus.cs
@@ -4,26 +4,109 @@ using System.Collections;
 public class guiStatus : MonoBehaviour {
 
 	private GameManager gameManager;
-	float health;
-	public Texture2D currentLife;
+	private int count;
+	private float health;
+	private float oldHealth;
+	private bool decreaseLife;
+	private bool increaseLife;
+	private int currentHealthFrame;
+	private object[] objects;
 	public float x = 0;
 	public float y = 0.9f;
 	GUITexture gt;
+	Texture[] decreaseLifeTextures;
+	Texture[] increaseLifeTextures;
 
 	void Start () {
 		GameObject controller = GameObject.Find("Controller");
 		gameManager = controller.GetComponent<GameManager> () as GameManager;
 		health = gameManager.showCurrentLife();
+		oldHealth = health;
 		gt = gameObject.GetComponent<GUITexture> () as GUITexture;
 		gt.transform.position = new Vector3(x,y,0);
 		gt.transform.localScale = Vector3.zero;
-		gt.texture = currentLife;
-		gt.pixelInset = new Rect(0, 0, Screen.width/2,Screen.width/20);
+
+		this.objects = Resources.LoadAll("LifeDecrease", typeof(Texture)); 
+
+		this.decreaseLifeTextures = new Texture[this.objects.Length];
+		for(int i=0; i < objects.Length;i++)  
+		{  
+			this.decreaseLifeTextures[i] = (Texture)this.objects[i];  
+		}   
+
+		this.objects = Resources.LoadAll("LifeIncrease", typeof(Texture)); 
+		
+		this.increaseLifeTextures = new Texture[this.objects.Length];
+		for(int i=0; i < objects.Length;i++)  
+		{  
+			this.increaseLifeTextures[i] = (Texture)this.objects[i];  
+		}   
+
+		gt.texture = this.decreaseLifeTextures[0];
+		gt.pixelInset = new Rect(-70, -200, 400,400);
+		currentHealthFrame = 0;
+
+		decreaseLife = false;
+		increaseLife = false;
 	}
 
 
 	void Update () {
+		float deltaHealth = 0;
 		health = gameManager.showCurrentLife();
-		gt.pixelInset = new Rect(0, 0, health*5,Screen.width/20);
+
+		if (health != oldHealth) {
+			deltaHealth = oldHealth-health;
+			if (deltaHealth > 0) {
+				decreaseLife = true;
+				count = 0;
+			}
+			else {
+				increaseLife = true;
+				count = 0;
+			}
+			oldHealth = health;
+		}
+
+		if (decreaseLife) {
+			decreaseLife = false;
+			StartCoroutine(DecreaseLife(0.05f,deltaHealth));
+		}
+
+		if (increaseLife){
+			increaseLife = false;
+			StartCoroutine(IncreaseLife(0.05f,deltaHealth));
+		}
+
+
+		gt.pixelInset = new Rect(-70, -200, 400,400);
+	}
+
+	IEnumerator DecreaseLife(float delay, float deltaHealth){
+		yield return new WaitForSeconds(delay);
+		int i = currentHealthFrame;
+		gt.texture = this.decreaseLifeTextures[i];
+		i++;
+		count ++;
+		currentHealthFrame = i;
+		decreaseLife = true;
+		if (48 == count) {
+			decreaseLife = false;	
+			i = 0;
+		}
+	}
+
+	IEnumerator IncreaseLife(float delay, float deltaHealth){
+		yield return new WaitForSeconds(delay);
+		int i = 193 - currentHealthFrame;
+		gt.texture = this.increaseLifeTextures[i];
+		i++;
+		count++;
+		currentHealthFrame = 193 - i;
+		increaseLife = true;
+		if (48 == count) {
+			increaseLife = false;	
+			i = 0;
+		}
 	}
 }
