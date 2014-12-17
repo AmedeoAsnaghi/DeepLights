facbceeeedd6429fbbe4e821c6cc4cf9a26c892b
AmedeoAsnaghi <amedeo.asnaghi@gmail.com>
2014-12-17 13:39:33 +0100

    Added damage warning

--- Assets/Gameplay/scripts/jellyfish/collisions.cs
+++ Assets/Gameplay/scripts/jellyfish/collisions.cs
@@ -5,12 +5,13 @@ public class collisions : MonoBehaviour {
 
 	Rigidbody2D rigidBody;
 	GameManager gameManager;
+	Animator anWarning;
 
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody2D> () as Rigidbody2D;
 		gameManager = GameObject.Find ("Controller").GetComponent<GameManager> () as GameManager;
-
+		anWarning = (GameObject.FindGameObjectWithTag ("warning")).GetComponent<Animator>() as Animator;
 	}
 	
 	// Update is called once per frame
@@ -22,22 +23,36 @@ public class collisions : MonoBehaviour {
 		Debug.Log (other.gameObject.tag);
 		if (other.gameObject.tag == "sphere") {
 			Destroy (other.gameObject);
-			gameManager.increaseLife(10);	
+			gameManager.increaseLife(20);	
 		} 
 		//TODO: manage other collision
 		else if (other.gameObject.tag == "Pincers") {
-			gameManager.decreaseLife(10);
+			gameManager.decreaseLife(20);
+			warning();
 		}
 
 		else if (other.gameObject.tag == "Predator") {
 			if (other.GetType() == typeof(BoxCollider2D))
 				gameManager.decreaseLife(20);
+			warning();
 		}
 		else if (other.gameObject.tag == "assassinAlga") {
 			if (other.GetType() == typeof(BoxCollider2D))
-				gameManager.decreaseLife(10);
+				gameManager.decreaseLife(20);
+			warning();
 		}
 
+	}
+
+	void warning(){
+		anWarning.SetBool ("warning", true);
+		StartCoroutine (WaitWarning());
+		//anWarning.SetBool ("warning", false);
+	}
+
+	IEnumerator WaitWarning(){
+		yield return new WaitForSeconds (2);
+		anWarning.SetBool ("warning", false);
 	}
 
 
