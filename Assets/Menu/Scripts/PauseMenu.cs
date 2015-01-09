a387f10ce1b1e615ad07039428c6ba54cca96294
Jacopo Fiorenza <jacopo.f91@gmail.com>
2015-01-10 00:09:40 +0100

    Added pause menu

---
+++ Assets/Menu/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	private bool pauseGame;
+	private bool showGUI;
+	private GameObject pauseGUI;
+	// Use this for initialization
+	void Start () {
+		pauseGame = false;
+		showGUI = false;
+		pauseGUI = GameObject.Find("PausedGUI");
+		pauseGUI.guiTexture.transform.position = new Vector3(0.35f,0.5f,0);
+		pauseGUI.guiTexture.transform.localScale = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("p"))
+		{
+			pauseGame = !pauseGame;
+			
+			if(pauseGame == true)
+			{
+				Time.timeScale = 0;
+				pauseGame = true;
+				showGUI = true;
+			}
+		}
+		
+		if(pauseGame == false)
+		{
+			Time.timeScale = 1;
+			pauseGame = false;
+			showGUI = false;
+		}
+		
+		if(showGUI == true)
+		{
+
+			pauseGUI.guiTexture.enabled = true;  
+
+		}
+		
+		else
+		{
+			pauseGUI.guiTexture.enabled = false;  
+		}
+	}
+}
