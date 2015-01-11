d65d76f7cde93c63fadb149f5b72943d7f12606e
Jacopo Fiorenza <jacopo.f91@gmail.com>
2015-01-11 17:47:19 +0100

    Added bomb

--- Assets/Gameplay/scripts/jellyfish/collisions.cs
+++ Assets/Gameplay/scripts/jellyfish/collisions.cs
@@ -5,13 +5,11 @@ public class collisions : MonoBehaviour {
 
 	Rigidbody2D rigidBody;
 	GameManager gameManager;
-	//Animator anWarning;
 
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody2D> () as Rigidbody2D;
 		gameManager = GameObject.Find ("Controller").GetComponent<GameManager> () as GameManager;
-		//anWarning = (GameObject.FindGameObjectWithTag ("warning")).GetComponent<Animator>() as Animator;
 	}
 	
 	// Update is called once per frame
@@ -22,33 +20,49 @@ public class collisions : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other){
 		Debug.Log (other.gameObject.tag);
 		if (other.gameObject.tag == "sphere") {
-						//Destroy (other.gameObject);
-						gameManager.increaseLife (20);
-						gameManager.showUpdatedScore ();
-
-				} 
+			gameManager.increaseLife (20);
+			gameManager.showUpdatedScore ();
+		} 
 		//TODO: manage other collision
 		else if (other.gameObject.tag == "Pincers") {
-						gameManager.decreaseLife (20);
-						//warning();
-				} else if (other.gameObject.tag == "Predator") {
-						if (other.GetType () == typeof(BoxCollider2D)) {
-								gameManager.decreaseLife (20);
-								//warning();
-						}
-
-				} else if (other.gameObject.tag == "assassinAlga") {
-						gameManager.decreaseLife (20);
-						//warning();
-				} else if (other.gameObject.tag == "melma") {
-						gameManager.decreaseLife(20);		
-		}
-		else if (other.gameObject.tag == "nextLevel") {
-			gameManager.changeLevel();
-			//warning();
+			gameManager.decreaseLife (20);
+		} 
+		else if (other.gameObject.tag == "Predator") {
+			if (other.GetType () == typeof(BoxCollider2D)) {
+				gameManager.decreaseLife (20);
+			}
+		} 
+		else if (other.gameObject.tag == "assassinAlga") {
+			gameManager.decreaseLife (20);
+		} 
+		else if (other.gameObject.tag == "melma") {
+			gameManager.decreaseLife(20);
+		} 
+		else if (other.gameObject.tag == "bomb") {
+			Animator an = other.GetComponent<Animator> () as Animator;
+			an.SetTrigger("bombTouched");
+			StartCoroutine(BombExplosionEffect(other.gameObject));
 		}
 
 	}
+
+	IEnumerator DestroyBomb(GameObject go) {
+		yield return new WaitForSeconds (0.5f);
+		Destroy (go);
+	}
+
+	IEnumerator BombExplosionEffect(GameObject go) {
+		yield return new WaitForSeconds (0.5f);
+		if (go != null) {
+			CircleCollider2D cc = go.GetComponent<CircleCollider2D>() as CircleCollider2D;
+			cc.radius += 1;
+			gameManager.decreaseLife(20);
+			StartCoroutine (DestroyBomb (go));
+		}
+
+	}
+
+
 
 
 
