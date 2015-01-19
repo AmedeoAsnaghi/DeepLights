a95c0eef7175d44172ab45a79940646848bf840f
Jacopo Fiorenza <jacopo.f91@gmail.com>
2015-01-19 11:01:50 +0100

    Added script for joystick movement on single analog

---
+++ Assets/Gameplay/Scripts/Jellyfish/JoystickMovementSingleAnalog.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoystickMovementSingleAnalog : MonoBehaviour {
+		
+	private GameManager gameManager;
+	Animator an = null;
+	bool i_am_moving = false;
+	Rigidbody2D rigidBody;
+	private Vector3 newAngle;
+	
+	public float rotationSpeed = 2f;
+	public float accellerationStep = 0.03f;
+	public float speed = 0.01f;
+	public float maxSpeed = 2f;
+	
+	private bool canMove;
+	
+	#region READ_KEYS
+	float getTurn() {
+		return Input.GetAxis("Horizontal");
+	}
+	
+	float getThrust(){
+		return Input.GetAxis("Vertical");
+	}
+	
+	bool getLightImpulse(){
+		return Input.GetButtonDown("LightImpulse");
+	}
+	bool getBarrier(){
+		return Input.GetButtonDown ("Barrier");
+	}
+	bool getFlash(){
+		return Input.GetButtonDown ("Flash");
+	}
+	#endregion
+	
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> () as Rigidbody2D;
+		an = GetComponent<Animator> () as Animator;
+		GameObject controller = GameObject.Find("Controller");
+		gameManager = controller.GetComponent<GameManager> () as GameManager;
+		newAngle = rigidBody.transform.eulerAngles;
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+		var currentState  = an.GetCurrentAnimatorStateInfo(0);	
+
+		/*----GO STRAIGHT AND TURN----*/
+		if (getThrust ()!=0 || getTurn() != 0)
+		{
+
+			if ((currentState.nameHash == Animator.StringToHash ("Base Layer.Moving")) && rigidBody.velocity.sqrMagnitude < (Vector3.one * maxSpeed).sqrMagnitude)
+				rigidBody.AddForce (new Vector2(getTurn() * speed, getThrust() * speed), ForceMode2D.Impulse);
+			if ((currentState.nameHash == Animator.StringToHash ("Base Layer.Charging")) && rigidBody.velocity.sqrMagnitude < (Vector3.one * maxSpeed).sqrMagnitude)
+				rigidBody.AddForce (-transform.up * speed);
+			if (!i_am_moving) {
+				i_am_moving = true;
+				an.SetBool ("is_moving", true);
+			}
+
+			var angleRadians=Mathf.Atan2(getThrust(), getTurn());
+			var angleDegrees = angleRadians * Mathf.Rad2Deg - 90;
+
+			if (angleDegrees<0) {
+				angleDegrees = 360 + angleDegrees;
+			}
+
+			newAngle = new Vector3(0,0,angleDegrees);
+			//var newZ = Mathf.Lerp(rigidBody.transform.eulerAngles,newAngle,Time.deltaTime * 2f);
+
+			//rigidBody.transform.eulerAngles =	rigidBody.transform.eulerAngles,newAngle,Time.deltaTime * 2f);
+
+			transform.rotation = Quaternion.Lerp(rigidBody.transform.rotation, Quaternion.Euler(newAngle),Time.deltaTime * rotationSpeed);	
+
+		}
+		else {
+			if (i_am_moving) {
+				i_am_moving = false;
+				an.SetBool ("is_moving", false);
+			}
+		}
+
+		
+		//-------POWERS-------
+		if (getLightImpulse ()) {
+			gameManager.doLightImpulse ();
+		}
+		if (getBarrier ()) {
+			gameManager.doBarrier();
+		}
+		if (getFlash ()) {
+			gameManager.doFlash();		
+		}
+	}
+}
