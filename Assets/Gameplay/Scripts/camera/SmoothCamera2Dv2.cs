b75fd7cd00d978ac6ddb2838a142ce77d218e75b
AmedeoAsnaghi <amedeo.asnaghi@gmail.com>
2015-01-19 18:24:13 +0100

    Added first level animation

--- Assets/Gameplay/Scripts/camera/SmoothCamera2Dv2.cs
+++ Assets/Gameplay/Scripts/camera/SmoothCamera2Dv2.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SmoothCamera2D : MonoBehaviour {
-	
+public class SmoothCamera2Dv2 : MonoBehaviour {
+
 	public float dampTime = 0.15f;
 	private Vector3 velocity = Vector3.zero;
 	public Transform target;
-
+	public bool init;
+	private bool firstTime = true;
 	// Update is called once per frame
 	void Update () 
 	{
-		if (target)
+		if (target && !init)
 		{
+			if(firstTime){
+				Animator anCamera = gameObject.GetComponent<Animator>() as Animator;
+				if(anCamera){
+					anCamera.enabled = false;
+				}
+				firstTime = false;
+			}
 			Vector3 point = camera.WorldToViewportPoint(target.position);
 			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
 			Vector3 destination = gameObject.transform.position + delta;
