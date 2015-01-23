08a00d77d6ad9611a539377dfc7dd77a5c6db8e0
And914 <andre@MBP-di-Andre.lan>
2015-01-23 09:01:21 +0100

    credits prefab

---
+++ Assets/Menu/Scripts/Credits.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+	public int speed;
+	public GameObject list;
+
+	// Update is called once per frame
+	void Update () {
+		list.transform.Translate (Vector3.up * speed * Time.deltaTime);
+	}
+}
